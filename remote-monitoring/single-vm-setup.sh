ca5dbecf6b1158bee12091b104dab00e7770faae
Devis Lucato <devis@lucato.it>
2017-09-03 02:13:14 -0700

    Refactor single-vm-setup.sh

--- remote-monitoring/single-vm-setup.sh
+++ remote-monitoring/single-vm-setup.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-DEST="${HOME}/docker"
+DEST="/app"
 
 mkdir -p ${DEST}
 cd ${DEST}
@@ -27,26 +27,26 @@ COMPOSEFILE="https://raw.githubusercontent.com/Azure/azure-iot-pcs-tools/master/
 
 wget $COMPOSEFILE -O ${DEST}/docker-compose.yml
 
-> ${DEST}/.env
-echo "HOST_NAME=${HOST_NAME}"     >> .env
-echo "APP_RUNTIME=${APP_RUNTIME}" >> .env
-echo "PCS_AUTH_AAD_GLOBAL_TENANTID=${PCS_AUTH_AAD_GLOBAL_TENANTID}" >> .env
-echo "PCS_AUTH_AAD_GLOBAL_CLIENTID=${PCS_AUTH_AAD_GLOBAL_CLIENTID}" >> .env
-echo "PCS_AUTH_AAD_GLOBAL_LOGINURI=${PCS_AUTH_AAD_GLOBAL_LOGINURI}" >> .env
-echo "PCS_IOTHUB_CONNSTRING=${PCS_IOTHUB_CONNSTRING}"               >> .env
-echo "PCS_STORAGEADAPTER_DOCUMENTDB_CONNSTRING=${PCS_STORAGEADAPTER_DOCUMENTDB_CONNSTRING}"   >> .env
-echo "PCS_DEVICETELEMETRY_DOCUMENTDB_CONNSTRING=${PCS_DEVICETELEMETRY_DOCUMENTDB_CONNSTRING}" >> .env
-echo "PCS_STREAMANALYTICS_DOCUMENTDB_CONNSTRING=${PCS_STREAMANALYTICS_DOCUMENTDB_CONNSTRING}" >> .env
-echo "PCS_IOTHUBREACT_ACCESS_CONNSTRING=${PCS_IOTHUBREACT_ACCESS_CONNSTRING}" >> .env
-echo "PCS_IOTHUBREACT_HUB_NAME=${PCS_IOTHUBREACT_HUB_NAME}"                   >> .env
-echo "PCS_IOTHUBREACT_HUB_ENDPOINT=${PCS_IOTHUBREACT_HUB_ENDPOINT}"           >> .env
-echo "PCS_IOTHUBREACT_HUB_PARTITIONS=${PCS_IOTHUBREACT_HUB_PARTITIONS}"       >> .env
-echo "PCS_IOTHUBREACT_AZUREBLOB_ACCOUNT=${PCS_IOTHUBREACT_AZUREBLOB_ACCOUNT}" >> .env
-echo "PCS_IOTHUBREACT_AZUREBLOB_KEY=${PCS_IOTHUBREACT_AZUREBLOB_KEY}"         >> .env
+touch ${DEST}/run.sh
+chmod 755 ${DEST}/run.sh
 
-> ${DEST}/run.sh
-echo "cd ${DEST}"        >> ${DEST}/run.sh
-echo "docker-compose up" >> ${DEST}/run.sh
-chmod 750 ${DEST}/run.sh
+echo "export HOST_NAME=\"${HOST_NAME}\""                                                                 >> ${DEST}/run.sh
+echo "export APP_RUNTIME=\"${APP_RUNTIME}\""                                                             >> ${DEST}/run.sh
+echo "export PCS_AUTH_AAD_GLOBAL_TENANTID=\"${PCS_AUTH_AAD_GLOBAL_TENANTID}\""                           >> ${DEST}/run.sh
+echo "export PCS_AUTH_AAD_GLOBAL_CLIENTID=\"${PCS_AUTH_AAD_GLOBAL_CLIENTID}\""                           >> ${DEST}/run.sh
+echo "export PCS_AUTH_AAD_GLOBAL_LOGINURI=\"${PCS_AUTH_AAD_GLOBAL_LOGINURI}\""                           >> ${DEST}/run.sh
+echo "export PCS_IOTHUB_CONNSTRING=\"${PCS_IOTHUB_CONNSTRING}\""                                         >> ${DEST}/run.sh
+echo "export PCS_STORAGEADAPTER_DOCUMENTDB_CONNSTRING=\"${PCS_STORAGEADAPTER_DOCUMENTDB_CONNSTRING}\""   >> ${DEST}/run.sh
+echo "export PCS_DEVICETELEMETRY_DOCUMENTDB_CONNSTRING=\"${PCS_DEVICETELEMETRY_DOCUMENTDB_CONNSTRING}\"" >> ${DEST}/run.sh
+echo "export PCS_STREAMANALYTICS_DOCUMENTDB_CONNSTRING=\"${PCS_STREAMANALYTICS_DOCUMENTDB_CONNSTRING}\"" >> ${DEST}/run.sh
+echo "export PCS_IOTHUBREACT_ACCESS_CONNSTRING=\"${PCS_IOTHUBREACT_ACCESS_CONNSTRING}\""                 >> ${DEST}/run.sh
+echo "export PCS_IOTHUBREACT_HUB_NAME=\"${PCS_IOTHUBREACT_HUB_NAME}\""                                   >> ${DEST}/run.sh
+echo "export PCS_IOTHUBREACT_HUB_ENDPOINT=\"${PCS_IOTHUBREACT_HUB_ENDPOINT}\""                           >> ${DEST}/run.sh
+echo "export PCS_IOTHUBREACT_HUB_PARTITIONS=\"${PCS_IOTHUBREACT_HUB_PARTITIONS}\""                       >> ${DEST}/run.sh
+echo "export PCS_IOTHUBREACT_AZUREBLOB_ACCOUNT=\"${PCS_IOTHUBREACT_AZUREBLOB_ACCOUNT}\""                 >> ${DEST}/run.sh
+echo "export PCS_IOTHUBREACT_AZUREBLOB_KEY=\"${PCS_IOTHUBREACT_AZUREBLOB_KEY}\""                         >> ${DEST}/run.sh
+echo                             >> ${DEST}/run.sh
+echo "cd ${DEST}"                >> ${DEST}/run.sh
+echo "nohup docker-compose up &" >> ${DEST}/run.sh
 
 nohup ${DEST}/run.sh &
