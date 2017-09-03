1f0b5d450e6858e247007104c9184e0d99172a3a
Devis Lucato <devis@lucato.it>
2017-09-03 13:22:38 -0700

    Add script to stop containers

--- remote-monitoring/single-vm-setup.sh
+++ remote-monitoring/single-vm-setup.sh
@@ -3,9 +3,8 @@
 set -x
 
 DEST="/app"
-
-mkdir -p ${DEST}
-cd ${DEST}
+START="${DEST}/start.sh"
+STOP="${DEST}/stop.sh"
 
 export HOST_NAME="${1:-localhost}"
 export APP_RUNTIME="${3:-dotnet}"
@@ -25,28 +24,42 @@ export PCS_IOTHUBREACT_AZUREBLOB_KEY="${14}"
 
 COMPOSEFILE="https://raw.githubusercontent.com/Azure/azure-iot-pcs-tools/master/remote-monitoring/docker-compose.${APP_RUNTIME}.yml"
 
+# ========================================================================
+
+mkdir -p ${DEST}
+cd ${DEST}
+touch ${START} && chmod 750 ${START}
+touch ${STOP} && chmod 750 ${STOP}
 wget $COMPOSEFILE -O ${DEST}/docker-compose.yml
 
-touch ${DEST}/run.sh
-chmod 755 ${DEST}/run.sh
+# ========================================================================
 
-echo "export HOST_NAME=\"${HOST_NAME}\""                                                                 >> ${DEST}/run.sh
-echo "export APP_RUNTIME=\"${APP_RUNTIME}\""                                                             >> ${DEST}/run.sh
-echo "export PCS_AUTH_AAD_GLOBAL_TENANTID=\"${PCS_AUTH_AAD_GLOBAL_TENANTID}\""                           >> ${DEST}/run.sh
-echo "export PCS_AUTH_AAD_GLOBAL_CLIENTID=\"${PCS_AUTH_AAD_GLOBAL_CLIENTID}\""                           >> ${DEST}/run.sh
-echo "export PCS_AUTH_AAD_GLOBAL_LOGINURI=\"${PCS_AUTH_AAD_GLOBAL_LOGINURI}\""                           >> ${DEST}/run.sh
-echo "export PCS_IOTHUB_CONNSTRING=\"${PCS_IOTHUB_CONNSTRING}\""                                         >> ${DEST}/run.sh
-echo "export PCS_STORAGEADAPTER_DOCUMENTDB_CONNSTRING=\"${PCS_STORAGEADAPTER_DOCUMENTDB_CONNSTRING}\""   >> ${DEST}/run.sh
-echo "export PCS_DEVICETELEMETRY_DOCUMENTDB_CONNSTRING=\"${PCS_DEVICETELEMETRY_DOCUMENTDB_CONNSTRING}\"" >> ${DEST}/run.sh
-echo "export PCS_STREAMANALYTICS_DOCUMENTDB_CONNSTRING=\"${PCS_STREAMANALYTICS_DOCUMENTDB_CONNSTRING}\"" >> ${DEST}/run.sh
-echo "export PCS_IOTHUBREACT_ACCESS_CONNSTRING=\"${PCS_IOTHUBREACT_ACCESS_CONNSTRING}\""                 >> ${DEST}/run.sh
-echo "export PCS_IOTHUBREACT_HUB_NAME=\"${PCS_IOTHUBREACT_HUB_NAME}\""                                   >> ${DEST}/run.sh
-echo "export PCS_IOTHUBREACT_HUB_ENDPOINT=\"${PCS_IOTHUBREACT_HUB_ENDPOINT}\""                           >> ${DEST}/run.sh
-echo "export PCS_IOTHUBREACT_HUB_PARTITIONS=\"${PCS_IOTHUBREACT_HUB_PARTITIONS}\""                       >> ${DEST}/run.sh
-echo "export PCS_IOTHUBREACT_AZUREBLOB_ACCOUNT=\"${PCS_IOTHUBREACT_AZUREBLOB_ACCOUNT}\""                 >> ${DEST}/run.sh
-echo "export PCS_IOTHUBREACT_AZUREBLOB_KEY=\"${PCS_IOTHUBREACT_AZUREBLOB_KEY}\""                         >> ${DEST}/run.sh
-echo                             >> ${DEST}/run.sh
-echo "cd ${DEST}"                >> ${DEST}/run.sh
-echo "nohup docker-compose up &" >> ${DEST}/run.sh
+echo "export HOST_NAME=\"${HOST_NAME}\""                                                                 >> ${START}
+echo "export APP_RUNTIME=\"${APP_RUNTIME}\""                                                             >> ${START}
+echo "export PCS_AUTH_AAD_GLOBAL_TENANTID=\"${PCS_AUTH_AAD_GLOBAL_TENANTID}\""                           >> ${START}
+echo "export PCS_AUTH_AAD_GLOBAL_CLIENTID=\"${PCS_AUTH_AAD_GLOBAL_CLIENTID}\""                           >> ${START}
+echo "export PCS_AUTH_AAD_GLOBAL_LOGINURI=\"${PCS_AUTH_AAD_GLOBAL_LOGINURI}\""                           >> ${START}
+echo "export PCS_IOTHUB_CONNSTRING=\"${PCS_IOTHUB_CONNSTRING}\""                                         >> ${START}
+echo "export PCS_STORAGEADAPTER_DOCUMENTDB_CONNSTRING=\"${PCS_STORAGEADAPTER_DOCUMENTDB_CONNSTRING}\""   >> ${START}
+echo "export PCS_DEVICETELEMETRY_DOCUMENTDB_CONNSTRING=\"${PCS_DEVICETELEMETRY_DOCUMENTDB_CONNSTRING}\"" >> ${START}
+echo "export PCS_STREAMANALYTICS_DOCUMENTDB_CONNSTRING=\"${PCS_STREAMANALYTICS_DOCUMENTDB_CONNSTRING}\"" >> ${START}
+echo "export PCS_IOTHUBREACT_ACCESS_CONNSTRING=\"${PCS_IOTHUBREACT_ACCESS_CONNSTRING}\""                 >> ${START}
+echo "export PCS_IOTHUBREACT_HUB_NAME=\"${PCS_IOTHUBREACT_HUB_NAME}\""                                   >> ${START}
+echo "export PCS_IOTHUBREACT_HUB_ENDPOINT=\"${PCS_IOTHUBREACT_HUB_ENDPOINT}\""                           >> ${START}
+echo "export PCS_IOTHUBREACT_HUB_PARTITIONS=\"${PCS_IOTHUBREACT_HUB_PARTITIONS}\""                       >> ${START}
+echo "export PCS_IOTHUBREACT_AZUREBLOB_ACCOUNT=\"${PCS_IOTHUBREACT_AZUREBLOB_ACCOUNT}\""                 >> ${START}
+echo "export PCS_IOTHUBREACT_AZUREBLOB_KEY=\"${PCS_IOTHUBREACT_AZUREBLOB_KEY}\""                         >> ${START}
+echo                             >> ${START}
+echo "cd ${DEST}"                >> ${START}
+echo "nohup docker-compose up &" >> ${START}
 
-nohup ${DEST}/run.sh &
+# ========================================================================
+
+echo 'list=$(docker ps -aq)'   >> ${STOP}
+echo 'if [ -n "$list" ]; then' >> ${STOP}
+echo '    docker rm -f $list'  >> ${STOP}
+echo 'fi'                      >> ${STOP}
+
+# ========================================================================
+
+nohup ${START} &
